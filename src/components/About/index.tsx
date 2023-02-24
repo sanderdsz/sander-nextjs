28f693649968da47c0bdbe6b6143d5c691abb954
sanderdsz <sanderdsz@gmail.com>
2023-02-24 16:33:36 -0300

    chore: mobile header and about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,8 +1,11 @@
 import { Typewriter } from "../Typewriter/index"
+import UseDeviceSize from "../../utils/UseDeviceSize"
 
 import styles from "./styles.module.scss"
 
 const About = () => {
+  const [width, height] = UseDeviceSize()
+
   return (
     <div className={styles.about}>
       <div className={`${[styles["about__header"]]}`}>
@@ -13,9 +16,18 @@ const About = () => {
         <span>dos Santos Zuchinalli</span>
       </div>
       <span className={`${[styles["about__maintext"]]}`}>
-        I’m a software engineer specialized in building (occasionally designing)
-        excellent digital experiences, which I tend to switch between backend
-        and frontend technologies at {" "}
+        { width <= 768 ? (
+          <>
+            I’m a software engineer specialized in building (occasionally designing)
+            excellent digital experiences at {" "}
+          </>
+        ) : (
+          <>
+            I’m a software engineer specialized in building (occasionally designing)
+            excellent digital experiences, which I tend to switch between backend
+            and frontend technologies at {" "}
+          </>
+        ) }
       </span>
       <a
         target="_blank"
